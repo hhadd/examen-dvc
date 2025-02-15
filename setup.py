9a3c8df3dfb6455ec4e8e76d8fe08adab0bc4023
Ubuntu <ubuntu@ip-172-31-12-199.eu-west-1.compute.internal>
2025-02-15 13:52:48 +0000

    setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='exam_dvc',
+    version='0.1.0',
+    packages=find_packages(),
+    description='dvc and dagshub evaluation',
+    author='Hassan Haddouchin',
+    license='MIT',
+)
